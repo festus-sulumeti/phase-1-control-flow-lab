fc031ee07aaeada993a3052c1eed7c8d39940c48
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-20 00:10:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400)
+    return 'This one is on me!';
+  
+  
+  else if (distance > 2000 && distance < 2500) 
+    return 'I will gladly take your thirty bucks.';
+  
+  else if (distance > 2500) 
+    return 'No can do.';
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let message;
+  message = "";
+
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) { case 'generous': return 'Thank you so much.'; break; 
+
+  case 'not as generous': return 'Thank you.'; break; default: return 'Bye.'; break;}
 }
+
+
+console.log(scuberGreetingForFeet(199));
